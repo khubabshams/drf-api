40503deac0d3eeb46cd0f4a9c40100e9c23c8a92
Khubab <khubab220@gmail.com>
2023-04-28 07:38:44 +0100

    Add follower model

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    Follower model, related to 'owner' and 'post'.
+    'owner' and 'followed are a User instances.
+    'unique_together' makes sure a user can't follow the same user twice.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='followed')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
