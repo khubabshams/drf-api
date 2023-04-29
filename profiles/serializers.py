8fb135b032db0e4c90fb52472295c4bb54a2943e
Khubab <khubab220@gmail.com>
2023-04-29 06:48:00 +0100

    Add annotating fields (counts)

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,6 +7,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    follower_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -24,5 +27,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'content',
-            'image', 'is_owner', 'following_id'
+            'image', 'is_owner', 'following_id', 'posts_count', 'follower_count',
+            'following_count',
         ]
